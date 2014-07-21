49efbf07eb47cbe3169bb6b669567a01d8bfd344
dario-c <dariocavanillas@gmail.com>
2014-07-21 15:56:29 +0200

    Added some locations to the table and a Method called iron_find

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+	def self.iron_find(x)
+		Location.where(id:x).first
+	end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+	describe "#find_iron works the same as #find" do 
+
+	Location.create(name:"Test",city:"Guanajuato")
+	Location.create(name:"Test2",city:"Panama")
+
+		it "Is the same value" do
+				expect(Location.find(2)).to eq(Location.iron_find(2))
+		end
+	end
 end
